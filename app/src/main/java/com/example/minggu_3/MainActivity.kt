6d9abad713abe00bc4548c674ec8c1f0fcd8e9ff
Afiqgsr <afiqgaluhsetyar@gmail.com>
2025-03-20 15:02:38 +0700

    Room Database pada mainactivity

--- app/src/main/java/com/example/minggu_3/MainActivity.kt
+++ app/src/main/java/com/example/minggu_3/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.minggu_3
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
 import com.example.minggu_3.databinding.ActivityMainBinding
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
 
@@ -59,11 +61,22 @@ class MainActivity : AppCompatActivity() {
             scoreViewModel.resetScore()
         }
 
-        // Mengatur padding sesuai dengan insets sistem (opsional)
+        // Mengatur padding sesuai dengan insets sistem
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val db = ColorDatabase.getInstance(this)
+        val executor = Executors.newSingleThreadExecutor() // Buat background thread
+
+        executor.execute {
+            val colorRed = Color(hexColor = "#ff0000", name = "Red") // Jangan isi ID
+            db.ColorDao().insert(colorRed)
+
+            // Cek apakah data masuk
+            val colors = db.ColorDao().getAll()
+            Log.d("Database", "Colors in DB: $colors")
         }
     }
 }
